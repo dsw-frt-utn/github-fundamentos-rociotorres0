9c5038d21f2bfe748428cf81582dbebe01dc7087
Rocioo <RocioNoemiTorres@alu.frt.utn.edu.ar>
2026-04-30 20:57:03 -0300

    Finalizado Problema 1 - Clases Parciales

--- TrabajoPracticoFundamentos/Problema1/obj/Debug/net10.0/Problema1.AssemblyInfo.cs
+++ TrabajoPracticoFundamentos/Problema1/obj/Debug/net10.0/Problema1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Problema1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a172a65216b67f20aba6d8a7c17b2bce551c064")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bc90809d96c2f0c16c1322f6f06d4eeebb851d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Problema1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Problema1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
