2bc90809d96c2f0c16c1322f6f06d4eeebb851d5
Rocioo <RocioNoemiTorres@alu.frt.utn.edu.ar>
2026-04-30 20:46:19 -0300

    Finalizado Problema 1 - Clases Parciales

---
+++ TrabajoPracticoFundamentos/Problema1/ProductHelper.Methods.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Problema1
+{
+    // Debe ser public y partial
+    public partial class ProductHelper
+    {
+        public string ObtenerEtiquetaProducto(long code, string description, decimal price)
+        {
+            // Formato requerido: [code] description - price
+            return $"[{code}] {description} - {FormatearPrecio(price)}";
+        }
+    }
+}
---
+++ TrabajoPracticoFundamentos/Problema1/ProductHelper.Utils.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Problema1
+{
+    public partial class ProductHelper
+    {
+        // Método auxiliar para el formato de moneda
+        private string FormatearPrecio(decimal price)
+        {
+            return price.ToString("C");
+        }
+    }
+}
